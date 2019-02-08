3108d68470f8f211c85aeb3636979da11ba2eb85
vuvk <panknd@mail.ru>
2019-02-08 15:45:36 +0500

    New recursive function for delete files and folders with deleting objects from project (textures)

--- src/main/java/com/vuvk/n3d/utils/FileSystemUtils.java
+++ src/main/java/com/vuvk/n3d/utils/FileSystemUtils.java
@@ -19,10 +19,13 @@ package com.vuvk.n3d.utils;
 
 import com.vuvk.n3d.Const;
 import com.vuvk.n3d.Global;
+import com.vuvk.n3d.editor.forms.FormMain;
+import com.vuvk.n3d.resources.Texture;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FilenameUtils;
@@ -34,6 +37,14 @@ import org.apache.commons.io.FilenameUtils;
 public final class FileSystemUtils {
     private FileSystemUtils(){}
     
+    
+    /** получить расширение файла
+     * @param file Файл, расширение которого нужно узнать
+     * @return строка, содержащая расширение файла без точки, в нижнем регистре
+     */
+    public static String getFileExtension(Path file) {  
+        return getFileExtension(file.toFile());
+    }
     
     /** получить расширение файла
      * @param file Файл, расширение которого нужно узнать
@@ -83,7 +94,7 @@ public final class FileSystemUtils {
      * @param path Путь, в котором будет удалено всё, включая сам путь
      * @return true, если удалены, false - возникла ошибка
      */
-    public static boolean removeFiles(Path path) { 
+    /*public static boolean removeFiles(Path path) { 
         File fPath = path.toFile();
         if (fPath.isDirectory()) {            
             try {
@@ -96,5 +107,75 @@ public final class FileSystemUtils {
         } else {
             return fPath.delete();
         }
-    }    
+    }*/    
+    
+    /**
+     * Удалить файл с проверкой на тип объекта (с удалением из настроек проекта)
+     * @param path Путь, по которому располагается файл
+     * @return true, если удален, false - это не файл или возникла ошибка
+     */
+    static boolean deleteFile(Path path) {   
+        if (Files.exists(path) && !Files.isDirectory(path)) {
+            
+            // по расширению файла определяем что это
+            // для того, чтобы удалить файл из настроек проекта
+            switch (getFileExtension(path)) {
+                case "txr" :
+                    String filePath = getProjectPath(path);
+                    for (Iterator it = Texture.TEXTURES.iterator(); it.hasNext(); ) {
+                        if (((Texture)it.next()).getPath().equals(filePath)) {
+                            it.remove();
+                        }
+                    }
+                    FormMain.closeFormTextureEditor();
+                    break;
+            }
+
+            return path.toFile().delete();
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Рекурсивное удаление всех файлов и папок в пути
+     * @param path Путь, в котором будет удалено всё, включая сам путь
+     * @return true, если удалены, false - возникла ошибка
+     */
+    public static boolean recursiveRemoveFiles(Path path) {
+        // пробежаться по содержимому
+        boolean finished = true;        
+        File fPath = path.toFile();
+        // путь это папка?
+        if (fPath.isDirectory()) {
+            for (File file : fPath.listFiles()) {
+                if (file.isDirectory()) {
+                    if (!recursiveRemoveFiles(file.toPath())) {
+                        finished = false;
+                    }
+                } else {
+                    if (!deleteFile(path)) {
+                        finished = false;
+                    }
+                }
+            }
+            
+            // удалить сам путь
+            try {
+                Files.delete(path);
+            } catch (IOException ex) {
+                Logger.getLogger(FileSystemUtils.class.getName()).log(Level.SEVERE, null, ex);
+                MessageDialog.showException(ex);
+                finished = false;
+            }
+        // по пути передан файл
+        } else {
+            if (!deleteFile(path)) {
+                finished = false;
+            }
+        }        
+        
+        return finished;
+    }
+
 }
